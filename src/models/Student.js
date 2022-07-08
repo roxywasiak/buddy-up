d60ba848ffd122f3bf169b41c8d64921a6ccbd0d
amirtha-coder <amirtha.m.muthirulandi@gmail.com>
2022-07-08 17:57:24 +0100

    finished model for student

---
+++ src/models/Student.js
@@ -0,0 +1,87 @@
+// import sequelize
+// import bcrypt
+const { Model, DataTypes } = require("sequelize");
+
+const connection = require("../config/connection");
+// import hooks
+
+class Student extends Model {
+  // call a method that returns
+  //    id: this.id,
+  //       firstName: this.firstName,
+  //       lastName: this.lastName,
+  //       email: this.email,
+}
+
+const schema = {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true,
+    },
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  budget: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false,
+    validate: {
+      isDecimal: true,
+    },
+    references: {
+      model: Price,
+      key: "id",
+    },
+  },
+  location: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  isRemote: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+  lat: {
+    type: DataTypes.DECIMAL(15, 8),
+    allowNull: false,
+    validate: {
+      isDecimal: true,
+    },
+  },
+  long: {
+    type: DataTypes.DECIMAL(15, 8),
+    allowNull: false,
+    validate: {
+      isDecimal: true,
+    },
+  },
+};
+
+const options = {
+  sequelize: connection,
+  timestamps: true,
+  underscored: false,
+  freezeTableName: true,
+  modelName: "student",
+};
+
+Student.init(schema, options);
+
+module.exports = Student;
