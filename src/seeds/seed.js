24cd76b77d9969ec64e8f8580433b469dcb6173b
amirtha-coder <amirtha.m.muthirulandi@gmail.com>
2022-07-11 22:13:02 +0100

    added review json file

--- src/seeds/seed.js
+++ src/seeds/seed.js
@@ -1,9 +1,109 @@
 //require the files
+const connection = require("../config/connection");
+const {
+  Ad,
+  Review,
+  Price,
+  Response,
+  Student,
+  Subject,
+  Tutor,
+  TutorSubject,
+} = require("../models");
+
+const ad = require("./ad.json");
+const review = require("./review.json");
+const price = require("./price.json");
+const response = require("./response.json");
+const student = require("./student.json");
+const subject = require("./subject.json");
+const tutor = require("./tutor.json");
+const tutorSubject = require("./tutorSubject.json");
 
 //variables to store the table entity names
+const seedAds = async () => {
+  const promises = ad.map((ad) => Ad.create(ad));
+  await Promise.all(promises);
+  console.log("Successfully seeded ad");
+};
+
+const seedReviews = async () => {
+  const promises = Review.map((review) => Review.create(review));
+  await Promise.all(promises);
+  console.log("Successfully seeded reviews");
+};
+
+const seedPrices = async () => {
+  const promises = price.map((price) => Price.create(price));
+  await Promise.all(promises);
+  console.log("Successfully seeded prices");
+};
+
+const seedResponses = async () => {
+  const promises = response.map((response) => Response.create(response));
+  await Promise.all(promises);
+  console.log("Successfully seeded responses");
+};
+
+const seedStudents = async () => {
+  const promises = student.map((student) => Student.create(student));
+  await Promise.all(promises);
+  console.log("Successfully seeded students");
+};
+
+const seedSubjects = async () => {
+  const promises = subject.map((subject) => Subject.create(subject));
+  await Promise.all(promises);
+  console.log("Successfully seeded subjects");
+};
+
+const seedTutors = async () => {
+  const promises = tutor.map((tutor) => Tutor.create(tutor));
+  await Promise.all(promises);
+  console.log("Successfully seeded tutors");
+};
+
+const seedTutorSubject = async () => {
+  const promises = tutorSubject.map((tutorSubject) =>
+    TutorSubject.create(tutorSubject)
+  );
+  await Promise.all(promises);
+  console.log("Successfully seeded tutor subjects");
+};
 //promise/map/create
 //await
 //console.log if seeded
 
 //init function
-//try
+const init = async () => {
+  try {
+    console.log("Seeding database...");
+    await connection.sync({ force: true });
+
+    //seed all data from entities
+    await seedPrices();
+
+    await seedTutors();
+
+    await seedStudents();
+
+    await seedReviews();
+
+    await seedAds();
+
+    await seedSubjects();
+
+    await seedTutorSubject();
+
+    await seedResponses();
+
+    console.log("Seeding complete!!");
+  } catch (error) {
+    console.log(`[ERROR]: Failed to seed | ${error.message}`);
+  }
+
+  // kill node process
+  process.exit(0);
+};
+
+init();
