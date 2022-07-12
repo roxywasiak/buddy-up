b712c124cfca7047e306b75a634bdda5f5ed1410
surajverma2587 <surajverma2587@gmail.com>
2022-07-12 21:54:07 +0100

    minor refactor

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ require("dotenv").config();
 const path = require("path");
 const exphbs = require("express-handlebars");
 const express = require("express");
-
-const hbs = exphbs.create({});
+const session = require("express-session");
+const connectSessionSequelize = require("connect-session-sequelize");
 
 const connection = require("./config/connection");
 const routes = require("./routes");
-const session = require("express-session");
-const connectSessionSequelize = require("connect-session-sequelize");
+
+const PORT = process.env.PORT || 4000;
 
 const SequelizeStore = connectSessionSequelize(session.Store);
 
 const sessionOptions = {
-  secret: process.env.DB_SESSIONSECRET,
+  secret: process.env.DB_SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
   cookies: {
@@ -27,11 +27,12 @@ const sessionOptions = {
   }),
 };
 
-const PORT = process.env.PORT || 4000;
-
+const hbs = exphbs.create({});
 const app = express();
+
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
+
 app.use(session(sessionOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
