b55b4f0d044dc5abc7e750a424b74c042c41b5e5
Josh Holmes <josh@glowingmanagement.com>
2022-07-13 18:33:53 +0100

    get user inputs from form

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -4,7 +4,18 @@ const handleSignup = (event) => {
   console.log("Submitted");
   event.preventDefault();
   const firstName = $("#firstName").val();
+  const lastName = $("#lastName").val();
+  const email = $("#email").val();
+  const password = $("#password").val();
+  const confirmPassword = $("#confirmPassword").val();
+  const accountType = $("input[name='radio-choice']:checked").val();
+  const termsAndConditions = $("#termsAndConditions").is(":checked");
   console.log(firstName);
+  console.log(lastName);
+  console.log(email), console.log(password);
+  console.log(confirmPassword);
+  console.log(accountType);
+  console.log(termsAndConditions);
 };
 
-signupForm.submit(handleSignup);
+$("#signup-submit").click(handleSignup);
