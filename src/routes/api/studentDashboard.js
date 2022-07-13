73aa238b43023924ea1b7e1d2fcea286d4ec67d4
amirtha-coder <amirtha.m.muthirulandi@gmail.com>
2022-07-13 22:00:02 +0100

    added extra folders to routes

--- src/routes/api/studentDashboard.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-
-const { getDashboard } = require("../../controllers/api/dashboard");
-
-const router = Router();
-
-router.get("/", getDashboard);
-
-module.exports = router;
