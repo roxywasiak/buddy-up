09675e3657be48797626c0480e00618310da0a78
Tanveer Bassi <tanveer13a551@gmail.com>
2022-07-14 20:30:11 +0100

    response js updated

--- src/controllers/api/response.js
+++ src/controllers/api/response.js
@@ -1,8 +1,58 @@
-const { Student, Tutor, Ad } = require("../../models");
+const { Student, Tutor, Ad, Response } = require("../../models");
 
-const createResponse = async () => {};
+const createResponse = async () => {
+  try {
+    const id = req.sessions.user.id;
+    const userType = req.sessions.user.userType;
+    if (userType === "student") {
+      const data = await Student.findByPk(id);
+      if (data) {
+        //create response for student
+        await Response.create();
+      }
+      return res.json({ success: true, data });
+    }
+    if (userType === "tutor") {
+      const data = await Tutor.findByPk(id);
 
-const updateResponse = async () => {};
+      return res.json({ success: true, data });
+    }
+    if (userType !== "tutor" && userType !== "student") {
+      return res.status(500).json({ success: false });
+    }
+  } catch (error) {
+    console.log(`[ERROR]: Failed to get user| ${error.message}`);
+
+    return res.status(500).json({ success: false });
+  }
+};
+
+const updateResponse = async () => {
+  try {
+    const id = req.sessions.user.id;
+    const userType = req.sessions.user.userType;
+    if (userType === "student") {
+      const data = await Student.findByPk(id);
+      if (data) {
+        //update response
+        Response.update();
+      }
+      return res.json({ success: true, data });
+    }
+    if (userType === "tutor") {
+      const data = await Tutor.findByPk(id);
+
+      return res.json({ success: true, data });
+    }
+    if (userType !== "tutor" && userType !== "student") {
+      return res.status(500).json({ success: false });
+    }
+  } catch (error) {
+    console.log(`[ERROR]: Failed to get user| ${error.message}`);
+
+    return res.status(500).json({ success: false });
+  }
+};
 
 module.exports = {
   createResponse,
