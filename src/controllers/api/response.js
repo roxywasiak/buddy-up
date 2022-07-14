1d1a10fe3deb15de8d46b3648067d411276bae9a
Tanveer Bassi <tanveer13a551@gmail.com>
2022-07-14 20:22:30 +0100

    starter code for response.js

---
+++ src/controllers/api/response.js
@@ -0,0 +1,16 @@
+const { Student, Tutor, Ad } = require("../../models");
+
+const createResponse = async () => {};
+
+const updateResponse = async () => {};
+
+module.exports = {
+  createResponse,
+  updateResponse,
+};
+
+// student post a ad
+// view all ads - button on front
+
+// create response by id
+// get respone by id
