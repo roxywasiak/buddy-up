74c4233bef6df464e21c7e4d520d27b84ff70391
amirtha-coder <amirtha.m.muthirulandi@gmail.com>
2022-07-15 22:12:10 +0100

    added ts

--- src/controllers/api/tutorSubject.js
+++ src/controllers/api/tutorSubject.js
@@ -1,14 +1,31 @@
-const { TutorSubject } = require("../../models");
+const { TutorSubject, Subject } = require("../../models");
+
+const createSubject = async () => {
+  try {
+    const { userType, subjectName, details } = req.body;
+    if (userType === "tutor") {
+      await Subject.create({ subjectName, details });
+
+      res.status(200).json({ success: true });
+    }
+  } catch (error) {
+    console.log(`[ERROR]: Failed to create subject | ${error.message}`);
+
+    return res.status(500).json({ success: false });
+  }
+};
 
 const createTutorSubject = async (req, res) => {
   try {
-    const { subjectId } = req.body;
+    const { subjectId, userType } = req.body;
 
-    const userId = req.session.user.id;
+    if (userType === "tutor") {
+      const tutorId = req.session.user.id;
 
-    await TutorSubject.create({ userId, subjectId, level });
+      await TutorSubject.create({ tutorId, subjectId, level });
 
-    return res.json({ success: true });
+      return res.json({ success: true });
+    }
   } catch (error) {
     console.log(`[ERROR]: Failed to create TutorSubject | ${error.message}`);
 
