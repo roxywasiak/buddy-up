89171d7c723ad550b011eeddf4a56c37ec5c1318
zahrahaji10 <102627226+zahrahaji10@users.noreply.github.com>
2022-07-16 12:04:28 +0100

    fixed the html for handlebar

--- public/assets/js/ads.js
+++ public/assets/js/ads.js
@@ -1,14 +1,16 @@
 const { Subject } = require("../../../src/models");
-
+console.log("hi")
 // ~ DECLARATIONS
 const adsBtn = $("#ads-btn");
 const adsBudget = $("#ads-budget");
-// const adsPrice = $("#ads-price");
 const adSubject = $("#ads-subject");
 const adsDescription = $("#ads-description");
 const adsTitle = $("#ads-title");
 
+
+// fn to handle form submit 
 const handleFormSubmit = async (event) => {
+  console.log("clicked")
   // prevent url form default
   event.preventDefault();
 
@@ -18,41 +20,50 @@ const handleFormSubmit = async (event) => {
   const subject = $("#ads-subject").val();
   const budget = $("#ads-budget").val();
   const btnContainer = $("#btn-container");
-  //   const price = $("#ads-price").val();
 
   // verification user input for all fields
   if (title && description && subject && budget) {
-    try {
-      // get all subject
+    try{
       const getSubjects = () => {
-        // send query for all subjects 
-        const subjects = await Subject.findAll()
-      };
-
-      // create payload
-      const payload = {
-        description,
-        subject,
-        budget,
-      };
-
-      //   create response
-      const response = await fetch("/api/ad/", {
-        method: "POST",
-        body: JSON.stringify(payload),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      console.log(response);
-
-      const data = await response.json();
-
-      console.log(data);
-    } catch (error) {
+        // get request to api
+        const response = await fetch("/api/subject/", {
+          method: "GET",
+          body: JSON.stringify(),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        console.log(response)
+          // send query for all subjects 
+  
+          const subjects = await Subject.findAll()
+        };
+  
+        // create payload
+        const payload = {
+          description,
+          subject,
+          budget,
+        };
+  
+        //   create response
+        const response = await fetch("/api/ad/", {
+          method: "POST",
+          body: JSON.stringify(payload),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+  
+        const data = await response.json();
+  
+        console.log(data)
+        
+    }  catch (error) {
       console.log(`[ERROR]: Failed to create an ad | ${error.message}`);
-    }
-  } else {
+    };  
+  } 
+  else {
     btnContainer.append(`<div class="uk-alert-danger" uk-alert>
     <a class="uk-alert-close" uk-close></a>
     <p> Please complete all fields </p>
