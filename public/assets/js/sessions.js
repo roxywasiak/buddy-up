8e58269bb5a5a0d42bddcf31a0f8f0cd6fe55ff8
Tanveer Bassi <tanveer13a551@gmail.com>
2022-07-16 12:39:07 +0100

    sessions js updated

--- public/assets/js/sessions.js
+++ public/assets/js/sessions.js
@@ -1,1 +1,20 @@
-const 
+const tutorCard = $("#tutor-card");
+
+// responses - pull all the responses for user id
+
+// display ones that are complete as cards
+
+// init
+
+// id code from Amirtha
+
+// turn this rep - look through api/response
+const response = await fetch("/apiAuth/signup", {
+  method: "GET",
+  body: JSON.stringify(payload),
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+tutorCard();
