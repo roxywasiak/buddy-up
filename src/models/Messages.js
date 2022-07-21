6295f9dd8bff9d481a529b855f666df060c39045
Josh Holmes <josh@glowingmanagement.com>
2022-07-21 19:59:45 +0100

    add messages model

---
+++ src/models/Messages.js
@@ -0,0 +1,49 @@
+const { Model, DataTypes } = require("sequelize");
+
+const connection = require("../config/connection");
+// import references
+const Student = require("./Student");
+const Tutor = require("./Tutor");
+
+class Messages extends Model {}
+
+const schema = {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  studentSenderId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: Student,
+      key: "id",
+    },
+  },
+  tutorSenderId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: Tutor,
+      key: "id",
+    },
+  },
+  messageContent: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+};
+
+const options = {
+  sequelize: connection,
+  timestamps: true,
+  underscored: false,
+  freezeTableName: true,
+  modelName: "messages",
+};
+
+Messages.init(schema, options);
+
+module.exports = Messages;
