9289c1fb385ed8a9a775e04e79e74458fd4f3abb
roxywasiak <ruksclone@hotmail.com>
2022-07-22 22:54:39 +0100

    added date format

--- src/helpers/handlebarsHelpers.js
+++ src/helpers/handlebarsHelpers.js
@@ -1,5 +1,15 @@
+const dateFns = require("date-fns");
+
 const isEqualTo = (data, equalToString) => {
   return data === equalToString;
 };
 
-module.exports = { isEqualTo };
+const dateFormat = (date) => {
+  const formattedDate = dateFns.format(new Date(date), "do MMMM, yyyy");
+  return `Created: ${formattedDate}`;
+};
+
+module.exports = {
+  isEqualTo,
+  dateFormat,
+};
